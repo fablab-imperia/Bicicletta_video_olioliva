0ae2c273d1a86e80f6dbe70a02220ba96b114c94
MassiminoilTrace <24638827+MassiminoilTrace@users.noreply.github.com>
2018-10-14 12:24:14 +0200

    Aggiunte note licenza GPLv3 usata e copyright.

--- Arduino/Bicicletta_video_olioliva.ino
+++ Arduino/Bicicletta_video_olioliva.ino
@@ -1,3 +1,24 @@
+/*
+<Installazione interattiva pista ciclabile is an interactive installation to promote a cycle lane in Liguria.
+This program sends bike speed values to a pc>
+    Copyright (C) 2018 Massimo Gismondi, Stefano Semeria
+
+This file is part of Installazione interattiva pista ciclabile.
+
+    Installazione interattiva pista ciclabile is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Installazione interattiva pista ciclabile is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Keyboard.h>
 
 #define RAGGIO_RUOTA 1
